3b4688b31ed7b96e83eeea30a27a1980fda834e3
Gourav Garg <ggarg55@gmail.com>
2019-04-03 22:06:30 +0530

    Create Variables.sol

---
+++ Variables.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.23;
+
+contract VariableExamples {
+    address public owner = msg.sender;
+    bool isAvailble = true;
+    bytes32 public nameByte = "Test 1";
+    string public awesome = "Test 1";
+    uint8 number = 8;
+}
