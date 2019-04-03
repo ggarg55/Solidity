00db358a251eaac9d281eecd7c5fa1930057fd44
Gourav Garg <ggarg55@gmail.com>
2019-04-03 22:04:27 +0530

    Create Library.sol

---
+++ Library.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.23;
+
+library LibraryTest {
+    function division() public pure returns (uint) {
+        return 10 / 5;
+    }
+    
+    function multiply() public pure returns (uint) {
+        return 10 * 5;
+    }
+}
